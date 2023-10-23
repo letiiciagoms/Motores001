7f86fc5ec314dbd91b2024b55dede2b4b3e2caf5
letiiciagoms <gomes.freitas@escolar.ifrn.edu.br>
2023-10-23 14:22:35 -0300

    lala

--- Motores001/Assets/Scripits/CamaraFollow.cs
+++ Motores001/Assets/Scripits/CamaraFollow.cs
@@ -6,6 +6,8 @@ public class CamaraFollow : MonoBehaviour
 {
     public Transform alvo;
     public Vector3 offset;
+    public float sensib = 2;
+    private float vaw
     // Start is called before the first frame update
     void Start()
     {
